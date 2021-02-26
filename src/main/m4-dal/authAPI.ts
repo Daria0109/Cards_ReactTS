b2cfee993b33ff18f37c62bd9e9571c6656929c3
Daria0109 <da6ulya888@gmail.com>
2021-02-26 17:35:38 +0300

    Merge to master

--- src/main/m4-dal/authAPI.ts
+++ src/main/m4-dal/authAPI.ts
@@ -5,7 +5,7 @@ const baseLocalhostURL = 'http://localhost:7542/2.0/'
 const baseHerokuURL = 'https://neko-back.herokuapp.com/2.0/'
 
 const instance = axios.create({
-	baseURL: baseLocalhostURL,
+	baseURL: baseHerokuURL,
 	withCredentials: true,
 })
 
@@ -67,19 +67,3 @@ export const authAPI = {
 			.then(res => res.data)
 	},
 }
-
-
-
-
-// created: "2021-02-07T19:05:49.207Z"
-// email: "sergdiag19@gmail.com"
-// isAdmin: false
-// name: "sergdiag19@gmail.com"
-// publicCardPacksCount: 0
-// rememberMe: false
-// token: "44e9b950-6af2-11eb-a3aa-43f1dde77f4b"
-// tokenDeathTime: 1612898233573
-// updated: "2021-02-09T16:17:13.574Z"
-// verified: false
-// __v: 0
-// _id: "60203a0d5c268c2adcce842a"
